89bbf308d0b6bbc4c0ea25e40587aff6fe9b3dec
Medafu <justrayzzin@gmail.com>
2022-07-09 22:28:57 +0300

    UI Realization finished

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let text = document.querySelector('.text')
+let counter_div = document.querySelector('.counter')
+let pc = 0 //player counter
+let cc = 0 //comp counter
+let buttons = document.querySelectorAll('button')
+
 function GameParse(number) {
     switch(number) {
         case 1:
@@ -9,56 +15,45 @@ function GameParse(number) {
     }
 }
 
-function userInput() {
-    text = prompt("Enter a sign")
-    text = text.toUpperCase()
-
-    switch (text) {
-        case "ROCK":
-            return 1
-        case "PAPER":
-            return 2
-        case "SCISSORS":
-            return 3
-    }
-}
-
 function computerPlay() {
     // 1 - Rock, 2 - Paper, 3 - Scissors
     return Math.floor(Math.random() * (4 - 1) + 1)
 }
 
-function playRound(player, computer) {
-    if ((player == 1 && computer == 3) || player > computer) {
-        console.log("You Won! " + GameParse(player) + " beats " + GameParse(computer))
-        return 1
-    } else if (player == computer) {
-        console.log("Draw!")
-    } else {
-        console.log("You Lose! " + GameParse(computer) + " beats " + GameParse(player))
-        return 0
-    }
+function handleEvent(e) {
+    let playerSelection = parseInt(e.srcElement.id)
+    let compSelection = computerPlay()
+    playRound(playerSelection, compSelection)
 }
 
+function playRound(player, computer) {
 
-function Game() {
-    let playerCount = 0
-    let compCount = 0
-    for (let i = 0; i < 5; i++) {
-        playerSelection = userInput()
-        computerSelection = computerPlay()
-        let result = playRound(playerSelection, computerSelection)
-        if (result == 2) {
-            continue
-        } else if (result) {
-            playerCount++
-        } else {
-            compCount++
+    
+    if ((player == 1 && computer == 3) || player > computer) {
+        text.textContent = "You Won! " + GameParse(player) + " beats " + GameParse(computer)
+        pc += 1
+    } else if (player == computer) {
+        text.textContent = "Draw!"
+    } else {
+        text.textContent = "You Lose! " + GameParse(computer) + " beats " + GameParse(player)
+        cc += 1
+    }
+
+    if (pc == 5 || cc == 5) {
+        buttons.forEach((button) => {
+            button.removeEventListener('click', handleEvent)
+        })
+
+        if (pc == 5) {
+            return counter_div.textContent = "GAME OVER! PLAYER WON!"
+        } else if (cc == 5) {
+            return counter_div.textContent = "GAME OVER! COMPUTER WON!"
         }
     }
-
-    alert("Result: Player: " + playerCount + " Computer: " + compCount)
-    console.log("type Game() to start again")
+        
+    counter_div.textContent = `Player ${pc} : Computer ${cc}`
 }
 
-Game()
+buttons.forEach((button) => {
+    button.addEventListener('click', handleEvent)
+})
